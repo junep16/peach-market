099b570bdef541ed11dc67206bdd7492d223d456
searchmoon <moonding901212@gmail.com>
2022-01-13 08:51:27 +0900

    🔥 프로필 설정 페이지 삭제 후 병합

--- js/set_profile.js
+++
@@ -1,8 +0,0 @@
-const emailPw = document.querySelector(".email-pw");
-const profile = document.querySelector(".profile");
-
-document.querySelector(".next-btn").addEventListener("click", () =>{
-  emailPw.style.display = "none";
-  console.log('뭥미')
-  profile.style.display = "block";
-})
