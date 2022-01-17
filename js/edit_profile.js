7a9774be2caca0e113ebd14dcd13d41a33e42d18
junep16 <jiyunpark0816@gmail.com>
2022-01-18 02:10:56 +0900

    ✨ 유저 프로필 수정 구현

--- js/edit_profile.js
+++ js/edit_profile.js
@@ -6,7 +6,7 @@ const profileImage = document.querySelector(".profile-image");
 const userName = document.querySelector(".user-name"); 
 const userId = document.querySelector(".input-id"); 
 const userIntro = document.querySelector("#user-introduction"); 
-const submitButton = document.querySelector(".ms-disabled-button");  
+const submitButton = document.querySelector(".ms-button");  
 
 async function getUserProfile() {
   const res= await fetch (url + "/profile/hey_binky", {
@@ -17,12 +17,10 @@ async function getUserProfile() {
     }
   }) 
   const json = await res.json();  
-  const profile = json.profile; 
-  console.log(profile);  
+  const profile = json.profile;   
 
   profileImage.src = profile.image; 
-  userName.value = profile.username; 
-  console.log(userName); 
+  userName.value = profile.username;  
   userId.value = profile.accountname; 
   userIntro.value = profile.intro; 
 }
@@ -42,57 +40,67 @@ async function imageUpload(file){
   return profileImgName; 
 }
 
-// 프로필 수정내용 업데이트 및 전달
-async function updateProfile() {
+  // 프로필 이미지 프리뷰
   uploadProfileImage.addEventListener("change", async function(event) {
-    const file = uploadProfileImage.files;  
-    console.log(file);
+    const file = uploadProfileImage.files;   
     const imageUrls = []; 
     const imgurl = await imageUpload(file[0]); 
-    imageUrls.push(url+"/"+imgurl); 
-    console.log(imageUrls); 
+    imageUrls.push(url+"/"+imgurl);  
     profileImage.src = imageUrls; 
   }); 
   
-  // 유저 이름 validation (2~10자) **확인해야함
+  // 유저 이름 validation (2~10자)
   userName.addEventListener("change", function(event) { 
     if (userName.value.length === 0) {
-      document.querySelector(".input-error.name").classList.add("on");  
+      document.querySelector(".input-error.name").classList.add("on"); 
+      throw "글자수가 0입니다"; 
     } else if (userName.value.length < 2) {
       document.querySelector(".input-error.name").classList.add("on");
+      throw "글자수가 2보다 작습니다";  
     } else if (userName.value.length > 10) {
       document.querySelector(".input-error.name").classList.add("on");
+      throw "글자수가 10보다 큽니다";
+    } else {
+      document.querySelector(".input-error.name").classList.remove("on");
     }
   })
 
   // 유저 ID validation (영문, 숫자, 특수문자(.,_)만 사용 가능) 
   const IdRegEx = /[0-9a-zA-Z,._]/gm; 
   userId.addEventListener("change", function(event) {  
-    let Id = userId.value; 
-    console.log(IdRegEx);
+    let Id = userId.value;  
     let found = Id.match(IdRegEx); 
-    Id = found.join(""); 
-    console.log(Id); 
+    Id = found.join("");  
 
     if (userId.value === Id) {
-      console.log(Id)
-    } else {
+      document.querySelector(".input-error.id").classList.remove("on");
+    } else if (userId.value !== Id) { 
       document.querySelector(".input-error.id").classList.add("on");
-    }
+      throw "아이디가 유효하지 않습니다";
+    }  
   })
-  
-  const res = await fetch(url+"/user", {
-    method:"PUT", 
-    body:JSON.stringify({
-      "user": {
-        "username": userName.value,
-        "accountname": userId,
-        "intro": userIntro.value,
-        "image": imageUrls
-        }
+
+// 프로필 수정내용 전달
+async function updateProfile() {
+  submitButton.addEventListener("click", async (event) => {
+    const res = await fetch(url+"/user", {
+      method:"PUT", 
+      headers:{
+        "Authorization" : `Bearer ${token}`,
+        "Content-type" : "application/json"
+      },
+      body:JSON.stringify({
+        "user": {
+          "username": userName.value,
+          "accountname": userId,
+          "intro": userIntro.value,
+          "image": profileImage.src
+          }
+      })
     })
-  })
-  const json = await res.json(); 
-  console.log(json);  
-};
-submitButton.addEventListener("click", updateProfile); 
+    const json = await res.json(); 
+    console.log(json);  
+    // location.href = "/views/index.html";
+  }
+)};
+updateProfile(); 
