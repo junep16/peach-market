1391fe2ee59aa5a439eca2d2e8c8fcfa3ab63872
junep16 <jiyunpark0816@gmail.com>
2022-01-24 16:28:07 +0900

    ✅  이미지 슬라이드 수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const token = localStorage.getItem("token"); 
 const postList = document.querySelector("main .post-lists"); 
 
+let slideWidth = 304; 
+let slideMargin = 20;
+
 // 1. 유저 팔로워 피드 받아오기  
 async function getPosts() { 
   const url = "http://146.56.183.55:5050"
@@ -44,11 +47,11 @@ async function getPosts() {
             <div class="post-img-button-wrap">
               <button class="img-slide one on" type="button"></button>
               <button class="img-slide two" type="button"></button>
-              <button class="img-slide three" type="button"></button></div>
+              <button class="img-slide three" type="button"></button>
             </div>
           </div>
         </div>
-      </div> 
+      </div>
       <div class="post-button-wrap">
         <button type="button" class="heart-btn" data-count="58">
           <span class="text-hide">좋아요</span>
@@ -61,23 +64,32 @@ async function getPosts() {
       </div>
       <span class="upload-date">${postDate.slice(0,4)}년 ${postDate.slice(5,7)}월 ${postDate.slice(8,10)}일</span> 
     </li> 
-  `    
-    addPostImages(postImage, i); 
+  ` 
+    if(post.image) {
+      addPostImages(postImage, i); 
+      if (i === 3) {
+        console.log(postImage); 
+      }
+    }
+
   });  
   }
 
 // 2. 이미지 렌더링 하는 함수 (위 68번째 줄에서 실행)
 function addPostImages(eachpost, i) {  
-  const slides = document.querySelector(`#post${i}`);  
-  const li = document.createElement("li"); 
-  const img = document.createElement("img"); 
-  li.classList.add("post-img-wrap"); 
-  img.classList.add("post-img");  
+  const slides = document.querySelector(`#post${i}`);  //ul
+
   // 이미지가 하나일 경우
   if (eachpost.length === 1) {
+
+    const li = document.createElement("li"); 
+    const img = document.createElement("img"); 
+    li.classList.add("post-img-wrap"); 
+    img.classList.add("post-img");  
     img.src =`${eachpost[0]}`
     li.appendChild(img); 
     slides.appendChild(li); 
+
     const postImage = document.querySelector(".post-img"); 
     postImage.style.width = "304px"; 
     postImage.style.height = "228px"; 
@@ -87,12 +99,16 @@ function addPostImages(eachpost, i) {
 
   } //이미지 여러개일 경우
     else if (eachpost.length > 1) {
-    for(let j = 0; j <= eachpost.length - 1 ; j++) {  
-      const imgs = document.createElement("img"); 
-      imgs.classList.add("post-img"); 
-      li.appendChild(imgs); 
+    for(let j = 0; j < eachpost.length; j++) {   
+      const li = document.createElement("li"); 
+      const img = document.createElement("img"); 
+      li.classList.add("post-img-wrap");
+      img.classList.add("post-img"); 
+
+      li.appendChild(img); 
       slides.appendChild(li);  
-      imgs.src=`${eachpost[j]}`;  
+      img.src=`${eachpost[j]}`;  
+      console.log(slides); 
 
       // 이미지 스타일링
       const postImage = document.querySelector(".post-img"); 
@@ -102,65 +118,46 @@ function addPostImages(eachpost, i) {
     }  
   } else {
     console.log("이미지 없음"); 
-  }  
+  }   
+  slides.style.width = (slideWidth + slideMargin)*slides.childElementCount - slideMargin + "px"; 
 }
 
-// 3. 이미지 슬라이드  
-function handleImageSlide() { 
-  const buttonController = document.querySelector(".post-img-button-wrap");   
-  const buttonOne = document.querySelector(".img-slide.one"); 
-  const buttonTwo = document.querySelector(".img-slide.two"); 
-  const buttonThree = document.querySelector(".img-slide.three");
-
-  // 버튼 컨트롤 (누르면 각각 불이 들어오도록)
-  buttonController.addEventListener("click", event => { 
-    if(buttonOne.classList.contains("on")) {
-      buttonOne.classList.toggle("on");  
-    } else if (buttonTwo.classList.contains("on")) {
-      buttonTwo.classList.toggle("on");
-    } else if (buttonThree.classList.contains("on")){
-      buttonThree.classList.toggle("on");
-    }
-    event.target.classList.toggle("on");   
-  }); 
-
-  //
-  const slides = document.querySelector(".post-img-container"); 
-  const slide = document.querySelectorAll(".post-img-container li");  
-
-  let currentIndex = 0; 
-  let slideCount = slide.length;  
-  let slideWidth = 304; 
-  let slideMargin = 20;
-
-  slides.style.width = (slideWidth + slideMargin)*slideCount - slideMargin + "px"; 
-
-  function moveSlide(num) {
-    slides.style.left = -num * 324 + "px"; 
-    currentIndex = num; 
-  }
-
-  // 각각 버튼 누르면 움직이는 부분
-  buttonOne.addEventListener("click", event => { 
-    moveSlide(0);  
-  }); 
-  
-  buttonTwo.addEventListener("click", event => {
-    currentIndex = 0;
-    moveSlide(currentIndex +1);  
-  }); 
-  buttonThree.addEventListener("click", event => {
-    currentIndex = 1;
-    moveSlide(currentIndex +1);  
-  });  
+function moveSlide(num, slides) {
+  slides.style.left = -num * 324 + "px";  
 } 
+
+function imageSlideControl() {
+  postList.addEventListener("click", (event) => {
+    const currentNode = event.target; 
+    const currentClass = currentNode.className; 
+
+    if(currentNode.tagName === "BUTTON" && currentClass !== "more-btn") { 
+      const slides = currentNode.parentElement.previousElementSibling;  
+      const buttonList = currentNode.parentElement.children;   
+      
+      let moveIndex; 
+      for (let i = 0; i < buttonList.length; i++) { 
+        if(buttonList[i].classList.contains("on")) {
+          buttonList[i].classList.remove("on");  
+        }
+        else if (currentNode === buttonList[i]) {
+          moveIndex = i; 
+        } 
+      }
+      currentNode.classList.add("on");  
+      moveSlide(moveIndex, slides); 
+    }; 
+  }) 
+}
+imageSlideControl(); 
+
+
+// 3. 이미지 슬라이드  
 
 // 비동기적으로 각각의 함수 실행
 async function init() {
   // 포스팅 데이터 먼저 렌더
-  await getPosts(); 
-  // 이후 이미지 슬라이더 작동
-  // handleImageSlide(); 
+  await getPosts();  
 }
 init();
 
