cd8651d54f7379da8675fc0f45c804efee9072d6
Woosang <yws1502@naver.com>
2022-01-26 18:48:58 +0900

    ✨ 채팅룸 모달 애니매이션 추가 및 코드 리펙토링

--- js/chat_detail.js
+++ js/chat_detail.js
@@ -3,23 +3,16 @@ const settingMoreBtn = document.querySelector(".more-btn");
 const modal = document.querySelector(".modal");
 
 settingMoreBtn.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
+  modal.classList.remove("hidden");
+  modal.addEventListener("click", (event) => {
+    const currentNode = event.target;
+    if (currentNode.className === "modal") {
+      modal.classList.add("hidden");
+    } else if (currentNode.tagName === "BUTTON") {
+      location.href = "/views/chat_list.html";
+    }
+  })
 })
-
-
-// 설정 더보기 버튼 닫기
-const closeModalBtn = document.querySelector(".close-modal-btn");
-
-closeModalBtn.addEventListener("click", () => {
-  modal.classList.add("hidden");
-})
-
-// 채팅방 나가기 버튼
-const chatOutBtn = document.querySelector(".chat-out-btn");
-
-chatOutBtn.addEventListener("click", () => {
-  location.href = "/views/chat_list.html"
-});
 
 // 뒤로 가기 버튼
 const prevBtn = document.querySelector(".prev-btn");
